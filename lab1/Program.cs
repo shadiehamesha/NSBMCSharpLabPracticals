eb08686e4b9e28401dedf9a50eff05768a4be4e6
acer <shmdkj1@gmail.com>
2024-01-24 14:19:16 +0530

    Add project files.

---
+++ lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+            int width;
+            
+            Console.WriteLine("Enter the length");
+            length = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the width");
+            width = Convert.ToInt32(Console.ReadLine());
+
+            int area = length * width;
+            Console.WriteLine("area = " + area);
+            Console.ReadLine();
+
+
+        }
+    }
+}
