4d95e31abddcc8942f8a3d7c4630c3cc78f53632
Mudannayaka S H <132426805+shadiehamesha@users.noreply.github.com>
2024-02-07 10:56:04 +0530

    Add files via upload

---
+++ Q1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLab1
+{
+    //class Program
+    //{
+    //    static void calculateArea()
+    //    {
+    //        Console.WriteLine("Enter length");
+    //        int num1 = Convert.ToInt32(Console.ReadLine());
+
+    //        Console.WriteLine("Enter width");
+    //        int num2 = Convert.ToInt32(Console.ReadLine());
+
+    //        int sum = num1 * num2;
+    //        Console.WriteLine("Area: "+sum);
+    //    }
+    //    static void Main(string[] args)
+    //    {
+    //        bool isProgramRunning = true;
+    //        while (isProgramRunning)
+    //        {
+    //            Console.WriteLine("Select Operation");
+    //            Console.WriteLine("1. Calculate area of a rectangle");
+    //            Console.WriteLine("2. Quit");
+
+    //            int userInput = Convert.ToInt32(Console.ReadLine());
+
+    //            if (userInput == 1)
+    //            {
+    //                calculateArea();
+    //            }
+    //            else if (userInput == 2)
+    //            {
+    //                isProgramRunning = false;
+    //                continue;
+    //            }
+    //            else
+    //            {
+    //                Console.WriteLine("Invalid operation");
+    //            }
+    //        }
+    //    }
+    //}
+}
